73d60007776d589744166bf6f1026e7e57c83194
Simon Andersson <simon@hiddencode.me>
2019-01-28 22:21:17 +0100

    Updated brew and macos

--- brew.sh
+++ brew.sh
@@ -23,28 +23,13 @@ fi;
 brew install wget --with-iri
 
 brew install vim --override-system-vi
-brew install homebrew/dupes/grep
-brew install homebrew/dupes/openssh
-brew install homebrew/dupes/screen
 
-brew install nmap
-brew install ack
-brew install git
-brew install git-extras
 brew install imagemagick --with-webp
-brew install ssh-copy-id
 brew install node
 brew install npm
 brew install openssl
-brew install nginx
-brew install postgresql
-brew install python3
-brew install mongodb
-brew install redis
-brew install memcached
 brew install pgcli
 brew install go
-brew install gotags
 
 # Remove outdated versions from the cellar.
 brew cleanup
