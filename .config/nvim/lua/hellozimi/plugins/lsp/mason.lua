74a28b3f7e213e4de2585faaeec14f06c34b10f7
Simon Andersson <simon@moyst.tech>
2024-07-11 12:00:57 +0200

    updated dotfiles

--- .config/nvim/lua/hellozimi/plugins/lsp/mason.lua
+++ .config/nvim/lua/hellozimi/plugins/lsp/mason.lua
@@ -27,7 +27,7 @@ return {
     mason_lspconfig.setup({
       -- list of servers for mason to install
       ensure_installed = {
-        "vtsls",
+        "tsserver",
         "html",
         "cssls",
         "tailwindcss",
