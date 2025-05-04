82bf8925a50fc8e172971be4b299dddf4ca23be3
Simon Andersson <215466+hellozimi@users.noreply.github.com>
2025-05-04 12:12:35 +0200

    revert back to catppuccin

--- .config/nvim/lua/hellozimi/plugins/colorscheme.lua
+++ .config/nvim/lua/hellozimi/plugins/colorscheme.lua
@@ -1,49 +1,21 @@
 return {
-  "folke/tokyonight.nvim",
-  lazy = false,
+  "catppuccin/nvim",
+  name = "catppuccin",
   priority = 1000,
-  opts = {},
+  lazy = false,
   config = function()
-    require("tokyonight").setup({
-      -- use the night style
-      style = "night",
-      -- disable italic for functions
-      styles = {
-        keywords = { italic = false },
-        comments = { reverse = false, bold = true },
-        functions = { italic = false },
-        variables = { italic = false, bold = true },
+    require("catppuccin").setup({
+      flavour = "mocha",
+      background = {
+        light = "latte",
+        dark = "mocha",
       },
-      -- Change the "hint" color to the "orange" color, and make the "error" color bright red
-      on_colors = function(colors)
-        colors.hint = colors.orange
-        colors.error = "#ff0000"
-      end
+      styles = {
+        comments = {},
+        conditionals = {},
+      },
     })
-
-    vim.cmd([[colorscheme tokyonight ]])
+    vim.opt.background = 'light'
+    vim.cmd.colorscheme "catppuccin"
   end
 }
-
---return {
---  "catppuccin/nvim",
---  name = "catppuccin",
---  priority = 1000,
---  lazy = false,
---  config = function()
---    require("catppuccin").setup({
---      flavour = "mocha",
---      background = {
---        light = "latte",
---        dark = "mocha",
---      },
---      styles = {
---        comments = {},
---        conditionals = {},
---      },
---    })
---    --vim.opt.background = 'light'
---    --vim.cmd("colorscheme catppuccin-mocha")
---    vim.cmd([[colorscheme catppuccin]])
---  end
---}
