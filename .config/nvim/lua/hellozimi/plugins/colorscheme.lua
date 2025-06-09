79775137ba46f957104e914fe9e8a0706f1e5a76
Simon Andersson <215466+hellozimi@users.noreply.github.com>
2025-06-10 01:28:36 +0200

    kanagawa color scheme

--- .config/nvim/lua/hellozimi/plugins/colorscheme.lua
+++ .config/nvim/lua/hellozimi/plugins/colorscheme.lua
@@ -1,21 +1,29 @@
 return {
-  "catppuccin/nvim",
-  name = "catppuccin",
-  priority = 1000,
-  lazy = false,
+  "rebelot/kanagawa.nvim",
   config = function()
-    require("catppuccin").setup({
-      flavour = "mocha",
-      background = {
-        light = "latte",
-        dark = "mocha",
-      },
-      styles = {
-        comments = {},
-        conditionals = {},
-      },
-    })
-    vim.opt.background = 'light'
-    vim.cmd.colorscheme "catppuccin"
+    vim.cmd.colorscheme "kanagawa"
   end
 }
+
+
+-- return {
+--   "catppuccin/nvim",
+--   name = "catppuccin",
+--   priority = 1000,
+--   lazy = false,
+--   config = function()
+--     require("catppuccin").setup({
+--       flavour = "mocha",
+--       background = {
+--         light = "latte",
+--         dark = "mocha",
+--       },
+--       styles = {
+--         comments = {},
+--         conditionals = {},
+--       },
+--     })
+--     vim.opt.background = 'light'
+--     vim.cmd.colorscheme "catppuccin"
+--   end
+-- }
