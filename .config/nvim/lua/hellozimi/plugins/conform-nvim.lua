b19ef0ababbef62db5e0ba9e36ab0f8c66f26ddb
Simon Andersson <215466+hellozimi@users.noreply.github.com>
2026-04-02 17:37:01 +0200

    biome/prettier support

--- .config/nvim/lua/hellozimi/plugins/conform-nvim.lua
+++ .config/nvim/lua/hellozimi/plugins/conform-nvim.lua
@@ -1,4 +1,12 @@
-local prettier = { "prettierd" }
+local function biome_or_prettier()
+  -- Check for biome.json in the project root
+  local biome_json = vim.fn.glob(vim.fn.getcwd() .. "/biome.json")
+  if biome_json ~= "" then
+    return { "biome" }
+  end
+  return { "prettierd" }
+end
+
 return {
   "stevearc/conform.nvim",
   event = { "BufReadPre", "BufNewFile" },
@@ -7,23 +15,28 @@ return {
 
     conform.setup({
       formatters_by_ft = {
-        javascript = prettier,
-        typescript = prettier,
-        javascriptreact = prettier,
-        typescriptreact = prettier,
-        svelte = prettier,
-        css = prettier,
-        html = prettier,
-        json = prettier,
-        yaml = prettier,
-        markdown = prettier,
-        graphql = prettier,
-        liquid = prettier,
+        javascript = biome_or_prettier,
+        typescript = biome_or_prettier,
+        javascriptreact = biome_or_prettier,
+        typescriptreact = biome_or_prettier,
+        svelte = biome_or_prettier,
+        css = biome_or_prettier,
+        html = biome_or_prettier,
+        json = biome_or_prettier,
+        yaml = { "prettierd" },
+        markdown = { "prettierd" },
+        graphql = { "prettierd" },
+        liquid = { "prettierd" },
         go = { "goimports", "goimports-reviser", "gofumpt" },
       },
       formatters = {
         ["goimports"] = { command = "/Users/simon/go/bin/goimports" },
         ["goimports-reviser"] = { prepend_args = { "-set-alias" } },
+        ["biome"] = {
+          command = "biome",
+          args = { "format", "--stdin-file-path", "$FILENAME" },
+          stdin = true,
+        },
       },
       format_on_save = {
         lsp_fallback = false,
